73a8477aaadfb3e85b39d01b533f56c8e7f9ae56
crhntr <hunter.christopher@icloud.com>
2018-08-11 15:15:05 -0700

    Removed Some Non-Clarifying White Space

--- update.go
+++ update.go
@@ -127,16 +127,13 @@ func (update Update) Unset(key string) error {
 	return nil
 }
 
-//
 func (update *Update) UnmarshalJSON(buf []byte) error {
 	err := json.Unmarshal(buf, &update.doc)
-
 	for opkey, _ := range (*update).doc {
 		if !IsUpdateOperator(string(opkey)) {
 			return fmt.Errorf("[ %q ] is not a valid update operator", opkey)
 		}
 	}
-
 	return err
 }
 
